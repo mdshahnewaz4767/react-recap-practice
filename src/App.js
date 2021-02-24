2b51b96a38dc73aa747b7f7cb884bceac8b95570
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-24 23:52:01 +0600

    simple update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; //For logo
 import './App.css';
 
 function App() {
@@ -6,9 +6,36 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome</h1>
+        <Area name={peoples[0]}></Area>
+        <Area name={peoples[1]}></Area>
+        <Area name={peoples[2]}></Area>
+        <Area name={peoples[3]}></Area>
+        <Area name={peoples[4]}></Area>
       </header>
     </div>
   );
+}
+// const persons = {name: 'Shahnewaz', age: 20};
+
+// const {name, age} = persons;
+//Array Destructuring
+const peoples = ['Muhammad', 'Shahnewaz', 'Newaz', 'Khondokar', 'Shahriar'];
+
+// const singlePeople = peoples.map(people => people);
+
+function Area(props){
+  const areaStyle = {
+    backgroundColor: 'lightsalmon',
+    boxShadow: '5px 5px 10px gray',
+    width: '500px',
+    margin: '10px'
+  }
+  return (
+    <div style = {areaStyle}>
+      <h1>Name: {props.name}</h1>
+      <h4>Age: </h4>
+    </div>
+  )
 }
 
 export default App;
