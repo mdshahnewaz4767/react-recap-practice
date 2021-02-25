78fc6180bbe38181aba7618bc8fc551bc4034f0c
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-25 22:20:27 +0600

    show data using api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 // import logo from './logo.svg'; //For logo
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Users></Users>
         <MovieCounter></MovieCounter>
         <Area name={peoples[0]}></Area>
       </header>
     </div>
   );
+}
+
+//API
+function Users(){
+  const usersStyle = {
+    border: '3px solid green',
+    width: '500px'
+  }
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setUsers(data);
+    })
+  }, [])
+  return (
+    <div>
+      <h3 style={usersStyle}>Users Data: </h3>
+      <ul>
+        {
+         users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 // const persons = {name: 'Shahnewaz', age: 20};
 
